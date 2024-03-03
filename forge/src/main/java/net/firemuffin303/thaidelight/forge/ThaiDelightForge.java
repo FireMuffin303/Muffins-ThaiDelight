80a5ecc9dc0f935d587fef17c68d86c5eb4f9949
FireMuffin303 <firegame303@gmail.com>
2024-03-03 16:26:53 +0700

    fix datagen bs & forge lootable

--- forge/src/main/java/net/firemuffin303/thaidelight/forge/ThaiDelightForge.java
+++ forge/src/main/java/net/firemuffin303/thaidelight/forge/ThaiDelightForge.java
@@ -19,6 +19,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraft.world.inventory.MenuType;
@@ -102,6 +103,7 @@ public class ThaiDelightForge {
 
     public void registerEntitySpawn(SpawnPlacementRegisterEvent spawnPlacements){
         spawnPlacements.register(ModEntityTypes.FLOWER_CRAB,SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, FlowerCrabEntity::checkSpawnRules, SpawnPlacementRegisterEvent.Operation.AND);
+        spawnPlacements.register(ModEntityTypes.DRAGONFLY,SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Mob::checkMobSpawnRules, SpawnPlacementRegisterEvent.Operation.AND);
     }
 
     public void registerEvent(RegisterEvent registerEvent){
