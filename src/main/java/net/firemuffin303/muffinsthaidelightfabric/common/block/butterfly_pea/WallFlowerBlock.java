cd22c0fe2d7492c8b17d27790c13da8bcac6692d
FireMuffin303 <firegame303@gmail.com>
2026-01-18 10:04:46 +0700

    butterfly pea wall retexture

--- src/main/java/net/firemuffin303/muffinsthaidelightfabric/common/block/butterfly_pea/WallFlowerBlock.java
+++ src/main/java/net/firemuffin303/muffinsthaidelightfabric/common/block/butterfly_pea/WallFlowerBlock.java
@@ -1,9 +1,13 @@
 package net.firemuffin303.muffinsthaidelightfabric.common.block.butterfly_pea;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.mojang.logging.LogUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.*;
@@ -14,13 +18,25 @@ import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
 
 public class WallFlowerBlock extends HorizontalDirectionalBlock implements SimpleWaterloggedBlock, SuspiciousEffectHolder {
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
     private final MobEffect suspiciousStewEffect;
     private final int effectDuration;
+    private static final Map<Direction, VoxelShape> AABBS = Maps.newEnumMap(ImmutableMap.of(
+            Direction.NORTH, Block.box(0f, 0.0f, 14.0f, 16f, 16.0f, 16.0f),
+            Direction.SOUTH, Block.box(0f, 0.0f, 0.0f, 16f, 16.0f, 2.0f),
+            Direction.WEST, Block.box(14f, 0.0f, 0.0f, 16, 16.0f, 16f),
+            Direction.EAST, Block.box(0f, 0.0f, 0.0f, 2f, 16.0f, 16f))
+    );
+
+
     public WallFlowerBlock(Properties properties,MobEffect mobEffect,int effectDuration) {
         super(properties);
         this.registerDefaultState(this.defaultBlockState().setValue(WATERLOGGED, false).setValue(FACING, Direction.NORTH));
@@ -57,10 +73,17 @@ public class WallFlowerBlock extends HorizontalDirectionalBlock implements Simpl
     public BlockState getStateForPlacement(BlockPlaceContext blockPlaceContext) {
         LevelAccessor levelAccessor = blockPlaceContext.getLevel();
         BlockPos blockPos = blockPlaceContext.getClickedPos();
+        BlockState blockState = this.defaultBlockState();
 
-        return this.defaultBlockState()
-                .setValue(WATERLOGGED, Boolean.valueOf(levelAccessor.getFluidState(blockPos).getType() == Fluids.WATER))
-                .setValue(FACING, blockPlaceContext.getHorizontalDirection().getOpposite());
+        for(Direction direction : blockPlaceContext.getNearestLookingDirections()){
+            if(!direction.getAxis().isHorizontal()  || !blockState.setValue(FACING, direction.getOpposite()).canSurvive(levelAccessor, blockPos) ) continue;
+
+            return this.defaultBlockState()
+                    .setValue(WATERLOGGED, levelAccessor.getFluidState(blockPos).getType() == Fluids.WATER)
+                    .setValue(FACING, direction.getOpposite());
+        }
+
+        return null;
     }
 
     @Override
@@ -71,6 +94,11 @@ public class WallFlowerBlock extends HorizontalDirectionalBlock implements Simpl
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(WATERLOGGED, FACING);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        return AABBS.get(blockState.getValue(FACING));
     }
 
     @Override
