77ec2889cfdd5f77f5e07201a13c20f12e820c2c
FireMuffin303 <firegame303@gmail.com>
2026-02-04 16:56:10 +0700

    JEI rendering

--- src/main/java/net/firemuffin303/muffinsthaidelightfabric/integration/jei/FermentedFishRecipeCategory.java
+++ src/main/java/net/firemuffin303/muffinsthaidelightfabric/integration/jei/FermentedFishRecipeCategory.java
@@ -1,6 +1,5 @@
 package net.firemuffin303.muffinsthaidelightfabric.integration.jei;
 
-import com.google.common.collect.ImmutableList;
 import com.mojang.blaze3d.vertex.PoseStack;
 import mezz.jei.api.gui.ITickTimer;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
@@ -15,7 +14,6 @@ import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.firemuffin303.muffinsthaidelightfabric.ThaiDelight;
 import net.firemuffin303.muffinsthaidelightfabric.common.block.FermentedFishCauldronBlock;
 import net.firemuffin303.muffinsthaidelightfabric.registry.ModBlocks;
-import net.firemuffin303.muffinsthaidelightfabric.registry.ModItems;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.LightTexture;
@@ -24,10 +22,12 @@ import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Quaternionf;
@@ -39,12 +39,14 @@ public class FermentedFishRecipeCategory implements IRecipeCategory<FermentedFis
     private final ResourceLocation CONTAINER_LOCATION = ThaiDelight.modid("textures/gui/jei/fermented_fish_jei.png");
     private final IDrawable icon;
     private final IDrawable background;
-    private final IDrawableAnimated cauldron;
+    private final IDrawable time_icon;
+    private final IDrawable slot;
 
     public FermentedFishRecipeCategory(IGuiHelper iGuiHelper) {
-        this.icon = iGuiHelper.createDrawableItemStack(new ItemStack(ModItems.FERMENTED_FISH));
+        this.icon = iGuiHelper.createDrawableItemStack(new ItemStack(Items.CAULDRON));
         this.background = iGuiHelper.createDrawable(CONTAINER_LOCATION,0,0,154,65);
-        this.cauldron = new FermentedFishCauldronDrawable();
+        this.time_icon = iGuiHelper.createDrawable(CONTAINER_LOCATION,154,0,17,22);
+        this.slot = iGuiHelper.createDrawable(CONTAINER_LOCATION,154,22,18,18);
     }
 
     @Override
@@ -54,7 +56,7 @@ public class FermentedFishRecipeCategory implements IRecipeCategory<FermentedFis
 
     @Override
     public Component getTitle() {
-        return Component.translatable("item.muffins_thaidelight.fermented_fish");
+        return Component.translatable("emi.category.muffins_thaidelight.cauldron_crafting");
     }
 
     @Override
@@ -69,32 +71,60 @@ public class FermentedFishRecipeCategory implements IRecipeCategory<FermentedFis
 
     @Override
     public void setRecipe(IRecipeLayoutBuilder builder, FermentedFishDummy recipe, IFocusGroup focuses) {
-        builder.addSlot(RecipeIngredientRole.INPUT,22,33).addIngredients(Ingredient.of(ItemTags.FISHES));
-        builder.addSlot(RecipeIngredientRole.INPUT,98,14).addItemStack(new ItemStack(Items.BOWL));
-        builder.addSlot(RecipeIngredientRole.OUTPUT,123,33).addItemStack(new ItemStack(ModItems.FERMENTED_FISH));
+        if(!recipe.extraIngredient().isEmpty()){
+            builder.addSlot(RecipeIngredientRole.INPUT,68,4).addItemStack(recipe.extraIngredient());
+        }
+
+        builder.addSlot(RecipeIngredientRole.INPUT,22,33).addIngredients(Ingredient.of(recipe.ingredient));
+        builder.addSlot(RecipeIngredientRole.INPUT,98,14).addItemStack(new ItemStack(recipe.container));
+        builder.addSlot(RecipeIngredientRole.OUTPUT,123,33).addItemStack(new ItemStack(recipe.result));
     }
 
     @Override
     public void draw(FermentedFishDummy recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
-        this.cauldron.draw(guiGraphics);
+        this.slot.draw(guiGraphics,97,13);
+
+        recipe.renderExtra(guiGraphics,this.time_icon,this.slot);
+        recipe.cauldronRender.draw(guiGraphics);
     }
 
     @Override
     public List<Component> getTooltipStrings(FermentedFishDummy recipe, IRecipeSlotsView recipeSlotsView, double mouseX, double mouseY) {
-        if(ClientRenderUtils.isCursorInsideBounds(60,27,34,29,mouseX,mouseY) ||
-                ClientRenderUtils.isCursorInsideBounds(69,2,17,22,mouseX,mouseY)
-        ){
-            return ImmutableList.of(translateKey("cauldron1"),translateKey("cauldron2"),translateKey("cauldron3"));
+        if(ClientRenderUtils.isCursorInsideBounds(60,27,34,29,mouseX,mouseY)){
+            return recipe.tooltip();
         }
 
         return IRecipeCategory.super.getTooltipStrings(recipe, recipeSlotsView, mouseX, mouseY);
     }
 
-    private static MutableComponent translateKey(@NotNull String suffix) {
-        return Component.translatable( ThaiDelight.MOD_ID+ ".jei.fermented_fish." + suffix);
+    public static MutableComponent translateKey(@NotNull String suffix) {
+        return Component.translatable( ThaiDelight.MOD_ID+ ".jei.cauldron." + suffix);
     }
 
     public static class FermentedFishDummy{
+        public TagKey<Item> ingredient;
+        public ItemLike container;
+        public ItemLike result;
+        public IDrawable cauldronRender;
+
+        public FermentedFishDummy(TagKey<Item> ingredient, ItemLike container, ItemLike result,IDrawable cauldronRendering){
+            this.ingredient = ingredient;
+            this.container = container;
+            this.result = result;
+            this.cauldronRender = cauldronRendering;
+        }
+
+         public void renderExtra(GuiGraphics guiGraphics,IDrawable timeIcon,IDrawable slot){
+
+         }
+
+         public ItemStack extraIngredient(){
+            return ItemStack.EMPTY;
+         }
+
+         public List<Component> tooltip(){
+            return List.of();
+         }
 
     }
 
@@ -159,6 +189,39 @@ public class FermentedFishRecipeCategory implements IRecipeCategory<FermentedFis
                     animationValue == 0 ? f : 1,
                     animationValue == 0 ? g : 1,
                     animationValue == 0 ? h : 1, LightTexture.FULL_BRIGHT, OverlayTexture.NO_OVERLAY
+            );
+
+            poseStack.popPose();
+        }
+    }
+
+    public static class CoconutCauldronDrawable implements IDrawable{
+
+        @Override
+        public int getWidth() {
+            return 34;
+        }
+
+        @Override
+        public int getHeight() {
+            return 29;
+        }
+
+        @Override
+        public void draw(GuiGraphics guiGraphics, int xOffset, int yOffset) {
+            PoseStack poseStack = guiGraphics.pose();
+            poseStack.pushPose();
+
+
+            poseStack.scale(20f,20f,-20f);
+            poseStack.translate(3.08f,0.85,-10);
+            poseStack.rotateAround(new Quaternionf().rotateXYZ(0.33633232F, -2.7F, 3.1415927F),1,1,1);
+            BlockState blockState = ModBlocks.COCONUT_CAULDRON.defaultBlockState().setValue(FermentedFishCauldronBlock.LEVEL,3);
+            Minecraft.getInstance().getBlockRenderer().getModelRenderer().renderModel(
+                    guiGraphics.pose().last(),
+                    guiGraphics.bufferSource().getBuffer(Sheets.solidBlockSheet()),
+                    blockState,
+                    Minecraft.getInstance().getBlockRenderer().getBlockModel(blockState),1,1, 1, LightTexture.FULL_BRIGHT, OverlayTexture.NO_OVERLAY
             );
 
             poseStack.popPose();
--- src/main/java/net/firemuffin303/muffinsthaidelightfabric/integration/jei/ThaiDelightJEIIntegration.java
+++ src/main/java/net/firemuffin303/muffinsthaidelightfabric/integration/jei/ThaiDelightJEIIntegration.java
@@ -1,6 +1,8 @@
 package net.firemuffin303.muffinsthaidelightfabric.integration.jei;
 
+import com.google.common.collect.ImmutableList;
 import mezz.jei.api.IModPlugin;
+import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.registration.IGuiHandlerRegistration;
@@ -10,15 +12,21 @@ import mezz.jei.api.registration.IRecipeRegistration;
 import net.firemuffin303.muffinsthaidelightfabric.ThaiDelight;
 import net.firemuffin303.muffinsthaidelightfabric.client.sceens.MortarScreen;
 import net.firemuffin303.muffinsthaidelightfabric.registry.ModBlocks;
+import net.firemuffin303.muffinsthaidelightfabric.registry.ModItems;
 import net.firemuffin303.muffinsthaidelightfabric.registry.ModRecipes;
+import net.firemuffin303.muffinsthaidelightfabric.registry.ModTags;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 
 import java.util.List;
 import java.util.Objects;
+
+import static net.firemuffin303.muffinsthaidelightfabric.integration.jei.FermentedFishRecipeCategory.translateKey;
 
 public class ThaiDelightJEIIntegration implements IModPlugin {
     public static final RecipeType<FermentedFishRecipeCategory.FermentedFishDummy> FERMENTED_FISH = new RecipeType<>(ThaiDelight.modid("fermented_fish"), FermentedFishRecipeCategory.FermentedFishDummy.class);
@@ -40,7 +48,35 @@ public class ThaiDelightJEIIntegration implements IModPlugin {
     public void registerRecipes(IRecipeRegistration registration) {
         ClientLevel level = Objects.requireNonNull(Minecraft.getInstance().level);
         registration.addRecipes(MortarJEI.MORTAR,level.getRecipeManager().getAllRecipesFor(ModRecipes.MORTAR));
-        registration.addRecipes(FERMENTED_FISH, List.of(new FermentedFishRecipeCategory.FermentedFishDummy()));
+        registration.addRecipes(FERMENTED_FISH, List.of(
+                new FermentedFishRecipeCategory.FermentedFishDummy(ModTags.COMMON_RAW_FISHES,Items.BOWL, ModItems.FERMENTED_FISH,new FermentedFishRecipeCategory.FermentedFishCauldronDrawable()){
+                    @Override
+                    public void renderExtra(GuiGraphics guiGraphics, IDrawable timeIcon, IDrawable slot) {
+                        timeIcon.draw(guiGraphics,68,2);
+                    }
+
+                    @Override
+                    public List<Component> tooltip() {
+                        return ImmutableList.of(translateKey("fermented_fish1"),translateKey("fermented_fish2"),translateKey("fermented_fish3"));
+                    }
+                },
+                new FermentedFishRecipeCategory.FermentedFishDummy(ModTags.COCONUT,Items.GLASS_BOTTLE,ModItems.COCONUT_MILK_BOTTLE,new FermentedFishRecipeCategory.CoconutCauldronDrawable()){
+                    @Override
+                    public void renderExtra(GuiGraphics guiGraphics, IDrawable timeIcon, IDrawable slot) {
+                        slot.draw(guiGraphics,67,3);
+                    }
+
+                    @Override
+                    public ItemStack extraIngredient() {
+                        return new ItemStack(Items.WATER_BUCKET);
+                    }
+
+                    @Override
+                    public List<Component> tooltip() {
+                        return ImmutableList.of(translateKey("coconut_milk_bottle"));
+                    }
+                }
+        ));
         //registration.addIngredientInfo(new ItemStack(ModBlocks.PAPAYA_LOG), VanillaTypes.ITEM_STACK, Component.translatable("jei.info.papaya_log"));
     }
 
